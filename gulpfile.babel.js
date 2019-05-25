dc82797867d0f0552768e5bcc786210e4a0ea340
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-25 13:46:29 +0900

    #4.0 Gulp SASS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
@@ -13,6 +16,11 @@ const routes = {
   img: {
     src: "src/img/*",
     dest: "build/img"
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css"
   }
 };
 
@@ -24,8 +32,7 @@ const pug = () =>
 
 const clean = () => del(["build/"]);
 
-const webserver = () =>
-  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
 
 const img = () =>
   gulp
@@ -33,14 +40,21 @@ const img = () =>
     .pipe(image())
     .pipe(gulp.dest(routes.img.dest));
 
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const live = gulp.parallel([webserver, watch]);
 
