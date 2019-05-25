4f70d95fd4b7a83c1d2d0c6fab40927d56218ba0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-25 12:54:55 +0900

    #1.1 Pug Task part Two

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
+import del from "del";
 
 const routes = {
   pug: {
@@ -8,10 +9,16 @@ const routes = {
   }
 };
 
-export const pug = () =>
+const pug = () =>
   gulp
     .src(routes.pug.src)
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([pug]);
+const clean = () => del(["build"]);
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([prepare, assets]);
