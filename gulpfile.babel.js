574d77a764ef12a9a5b87b8b7de7d969047a6e6e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-25 12:48:23 +0900

    1.0 Pug Task part One

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,17 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
+
+const routes = {
+  pug: {
+    src: "src/*.pug",
+    dest: "build"
+  }
+};
+
+export const pug = () =>
+  gulp
+    .src(routes.pug.src)
+    .pipe(gpug())
+    .pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([pug]);
