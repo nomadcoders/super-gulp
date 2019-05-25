540b830cd05afb04468ecb903b55c2195f0f771f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-25 12:33:40 +0900

    #0.3 Scaffolding Part Two

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import { random } from "./util";
+
+const rOne = random(10);
+const rTwo = random(20);
+
+console.log(`${rOne} ${rTwo}`);
---
+++ src/js/util.js
@@ -0,0 +1,1 @@
+export const random = max => Math.floor(Math.random() * max);
