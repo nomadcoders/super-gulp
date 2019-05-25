09cdb549bb51cff417ac210bbb2f2f4bfc7fc115
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-25 13:07:39 +0900

    #2.1 Watching Files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import ws from "gulp-webserver";
 
 const routes = {
   pug: {
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build"
   }
@@ -21,10 +22,14 @@ const clean = () => del(["build/"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.series([webserver]);
+const postDev = gulp.series([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
